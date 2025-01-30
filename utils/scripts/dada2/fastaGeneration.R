78d3c72fd6c71a0401d3a8a51ce00c4310fd558d
saharbagheri <sahar.bagheri1@ucalgary.ca>
2025-01-30 13:56:45 -0700

    Update and rename alignment.R to fastaGeneration.R

--- utils/scripts/dada2/fastaGeneration.R
+++ utils/scripts/dada2/fastaGeneration.R
@@ -5,16 +5,10 @@ seqtab = readRDS(snakemake@input[['seqtab']])
 
 seqs <- colnames(seqtab)
 
-names(seqs) <- seqs # This propagates to the tip labels of the tree
+names(seqs) <- seqs 
 
 seq_stringset<-DNAStringSet(seqs)
 
 writeXStringSet(seq_stringset,snakemake@output[['seqfasta']])
 
 print("ASV sequences written to FASTA file")
-
-alignment <- AlignSeqs(DNAStringSet(seqs), anchor=NA)
-
-writeXStringSet(alignment,snakemake@output[['alignment']])
-
-print("Multiple sequence alignment written to FASTA file")
