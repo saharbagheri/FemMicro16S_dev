61d3d5778bb858fc03c06e933ec91d853f4e73de
saharbagheri <sahar.bagheri1@ucalgary.ca>
2025-02-21 16:07:48 -0700

    Update learnErrorRates.R

--- utils/scripts/dada2/learnErrorRates.R
+++ utils/scripts/dada2/learnErrorRates.R
@@ -1,17 +1,32 @@
 suppressMessages(library(dada2))
 suppressMessages(library(ggplot2))
 
+R1= snakemake@input[['R1']]
+R2= snakemake@input[['R2']]
 
-errF <- learnErrors(snakemake@input[['R1']], nbases=snakemake@config[["learn_nbases"]], multithread=snakemake@threads)
-errR <- learnErrors(snakemake@input[['R2']], nbases=snakemake@config[["learn_nbases"]], multithread=snakemake@threads)
+neg_samples <- unlist(base::strsplit(snakemake@params[['neg']], split = "\\|"))
+
+#Reading in negative samples to exclude them from the list of samples
+if (length(neg_samples) > 0) {
+  R1_files <- R1[!(basename(R1) %in% neg_samples)]
+  R2_files <- R2[!(basename(R2) %in% neg_samples)]
+} else {
+  R1_files <- R1
+  R2_files <- R2
+}
+
+
+errF <- learnErrors(R1_files, nbases=snakemake@config[["learn_nbases"]], multithread=snakemake@threads)
+errR <- learnErrors(R2_files, nbases=snakemake@config[["learn_nbases"]], multithread=snakemake@threads)
+
 save(errF,file=snakemake@output[['errR1']])
 save(errR,file=snakemake@output[['errR2']])
 
-
-
+  
 ## ---- plot-rates ----
 p<-plotErrors(errF,nominalQ=TRUE)
 ggsave(snakemake@output[['plotErr1']],plot=p)
+
 p<-plotErrors(errR,nominalQ=TRUE)
 ggsave(snakemake@output[['plotErr2']],plot=p)
 
