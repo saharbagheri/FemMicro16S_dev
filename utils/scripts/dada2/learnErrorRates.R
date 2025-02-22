f702dcd1cdd543ba014cd49c4af43215be0fb782
saharbagheri <sahar.bagheri1@ucalgary.ca>
2025-02-22 14:56:24 -0700

    Removing negative controls for learning error rates

--- utils/scripts/dada2/learnErrorRates.R
+++ utils/scripts/dada2/learnErrorRates.R
@@ -1,17 +1,18 @@
 suppressMessages(library(dada2))
 suppressMessages(library(ggplot2))
 
-R1= snakemake@input[['R1']]
-R2= snakemake@input[['R2']]
+R1= as.vector(snakemake@input[['R1']])
+R2= as.vector(snakemake@input[['R2']])
 
 neg_samples <- unlist(base::strsplit(snakemake@params[['neg']], split = "\\|"))
 
 message("Number of sample listed as negative: ", length(neg_samples))
 
+
 #Reading in negative samples to exclude them from the list of samples
 if (length(neg_samples) > 0) {
-  R1_files <- R1[!(basename(R1) %in% neg_samples)]
-  R2_files <- R2[!(basename(R2) %in% neg_samples)]
+  R1_files <- R1[!(sub("_R.*", "", sub(".*/", "", R1)) %in% neg_samples)]
+  R2_files <- R2[!(sub("_R.*", "", sub(".*/", "", R2)) %in% neg_samples)]
   message("Number of samples after excluding negative samples: ", length(R2_files))
 } else {
   R1_files <- R1
@@ -26,14 +27,10 @@ errR <- learnErrors(R2_files, nbases=snakemake@config[["learn_nbases"]], multith
 save(errF,file=snakemake@output[['errR1']])
 save(errR,file=snakemake@output[['errR2']])
 
-  
+
 ## ---- plot-rates ----
 p<-plotErrors(errF,nominalQ=TRUE)
 ggsave(snakemake@output[['plotErr1']],plot=p)
 
 p<-plotErrors(errR,nominalQ=TRUE)
 ggsave(snakemake@output[['plotErr2']],plot=p)
-
-
-
-
